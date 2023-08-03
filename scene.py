b1c5db87dfc367ba7156ca06b97d49965017dcb2
Pierre-Louis Ruhlmann <ruhlmannpl@gmail.com>
2023-08-03 16:19:54 +0100

    e

--- scene.py
+++ scene.py
@@ -14,6 +14,8 @@ class ExampleAtoms(Scene):
 
         vg = VGroup()
 
+        vg.add(axes, axes_labels)
+
         for i in range(20):
             x = i * 0.1
             fx = 0.5 * np.sin(2 * PI * x) + 2 * x
@@ -21,13 +23,7 @@ class ExampleAtoms(Scene):
             # dot.add_updater(lambda obj: obj.move_to(axes.coords_to_point(x, fx)))
             vg.add(dot)
 
-        self.add(axes)
-        self.play(Write(axes))
-        self.wait()
-        self.add(axes_labels)
-        self.play(FadeIn(axes_labels))
-        self.wait()
         self.play(Write(vg), run_time=3)
         self.wait(3)
-        self.play(axes.animate.move_to(LEFT))
+        self.play(vg.animate.scale(0.3).move_to(LEFT))
         self.wait(2)
