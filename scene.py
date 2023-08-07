09707ddfb2908c8a1d2d63f1a1de75b9e011f4e5
Pierre-Louis Ruhlmann <ruhlmannpl@gmail.com>
2023-08-07 15:04:53 +0100

    f

--- scene.py
+++ scene.py
@@ -5,8 +5,9 @@ class ExampleAtoms(Scene):
     def construct(self):
         axes = Axes(
             x_range=[0, 2.2, 0.2],
-            y_range=[0, 5, 1],
+            y_range=[0, 5, 0.5],
             x_length=10,
+            y_length=6,
             axis_config={"color": WHITE},
         )
 
@@ -16,16 +17,18 @@ class ExampleAtoms(Scene):
 
         vg.add(axes, axes_labels)
 
-        for i in range(20):
+        for i in range(30):
             x = i * 0.1
             fx = 0.5 * np.sin(2 * PI * x) + 2 * x
-            dot = Dot(axes.coords_to_point(x, fx), color=BLUE)
+            dot = Dot(
+                axes.coords_to_point(x, fx), color=BLUE, radius=DEFAULT_SMALL_DOT_RADIUS
+            )
             # dot.add_updater(lambda obj: obj.move_to(axes.coords_to_point(x, fx)))
             vg.add(dot)
 
-        self.play(Write(vg), run_time=3)
+        self.play(Write(vg), run_time=4)
         self.wait(1)
         self.play(vg.animate.scale(0.5))
         self.wait()
-        self.play(vg.animate.shift(6 * LEFT).scale_to_fit_width(0.7 * vg.width))
+        self.play(vg.animate.shift(4 * LEFT).scale_to_fit_width(0.7 * vg.width))
         self.wait(2)
