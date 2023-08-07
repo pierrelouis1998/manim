bee103b54323cc388a8aaf3554f8370de955091d
Pierre-Louis Ruhlmann <ruhlmannpl@gmail.com>
2023-08-07 15:24:49 +0100

    f

--- scene.py
+++ scene.py
@@ -27,9 +27,21 @@ class ExampleAtoms(Scene):
             # dot.add_updater(lambda obj: obj.move_to(axes.coords_to_point(x, fx)))
             vg.add(dot)
 
-        self.play(Write(vg), run_time=4, rate_functions=lambda t: t ** 3)
-        self.wait(1)
+        self.play(Write(vg), run_time=5, rate_functions=lambda t: t ** 3)
+        self.wait(2)
+        self.play(FadeOut(axes, axes_labels))
         self.play(vg.animate.scale(0.5))
         self.wait()
-        self.play(vg.animate.shift(4 * LEFT).scale_to_fit_width(0.7 * vg.width))
+        self.play(vg.animate.shift(4 * LEFT).scale_to_fit_width(0.8 * vg.width))
         self.wait(2)
+        equal = Tex("$=", font_size=30).next_to(vg, RIGHT, buff=RIGHT)
+        self.play(Write(equal))
+        lin_data = VGroup()
+        for i in range(npoints):
+            dx = vg[-1].get_x() - vg[-2].get_x()
+            x0 = equal.get_x() + RIGHT
+            y0 = vg[2].get_y()
+            lin_data.add(
+                Dot([x0, y0 + i * 2 * dx], color=YELLOW_B, radius=vg[-1].radius)
+            )
+        self.play(Write(lin_data))
